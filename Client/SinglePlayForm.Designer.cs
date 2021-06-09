89e78f2bf088fa1460634b68c17a9c8338eaf169
rudgjsdl123 <khunlee0508@gmail.com>
2021-06-10 00:14:27 +0900

    혼자하기 및 오목판 구성 구

---
+++ Client/SinglePlayForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Client
+{
+    partial class SinglePlayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boardPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.boardPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boardPicture
+            // 
+            this.boardPicture.BackColor = System.Drawing.SystemColors.Info;
+            this.boardPicture.Location = new System.Drawing.Point(12, 29);
+            this.boardPicture.Name = "boardPicture";
+            this.boardPicture.Size = new System.Drawing.Size(500, 500);
+            this.boardPicture.TabIndex = 0;
+            this.boardPicture.TabStop = false;
+            this.boardPicture.Paint += new System.Windows.Forms.PaintEventHandler(this.boardPicture_Paint);
+            this.boardPicture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.boardPicture_MouseDown);
+            // 
+            // SinglePlayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 588);
+            this.Controls.Add(this.boardPicture);
+            this.Name = "SinglePlayForm";
+            this.Text = "SinglePlayForm";
+            this.Load += new System.EventHandler(this.SinglePlayForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.boardPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox boardPicture;
+    }
+}
---
+++ Client/SinglePlayForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class SinglePlayForm : Form
+    {
+        private const int rectSize = 33; //오목판의 셀 크기
+        private const int edgeCount = 15; //오목판의 선 개수
+
+        private enum Horse { none=0,BLACK,WHITE};
+        private Horse[,] board = new Horse[edgeCount, edgeCount];
+        private Horse nowPlayer = Horse.BLACK;
+
+        public SinglePlayForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SinglePlayForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boardPicture_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.boardPicture.CreateGraphics();
+            int x = e.X / rectSize;
+            int y = e.Y / rectSize;
+            if (x<0 || y<0 || x>= edgeCount || y >= edgeCount)
+            {
+                MessageBox.Show("테두리를 벗어날 수 없습니다.");
+            }
+            MessageBox.Show(x + ", " + y);
+            if (nowPlayer == Horse.BLACK)
+            {
+                SolidBrush brush = new SolidBrush(Color.Black);
+                g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
+            }
+            else
+            {
+                SolidBrush brush = new SolidBrush(Color.White);
+                g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
+            }
+        }
+
+        private void boardPicture_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gp = e.Graphics;
+            Color lineColor = Color.Black; //오목판의 선 색깔
+            Pen p = new Pen(lineColor, 2);
+            gp.DrawLine(p, rectSize / 2, rectSize / 2, rectSize / 2, rectSize * edgeCount - rectSize / 2); //좌측
+            gp.DrawLine(p, rectSize / 2, rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize / 2); //상측
+            gp.DrawLine(p, rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2); //하측
+            gp.DrawLine(p, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2, rectSize * edgeCount - rectSize / 2); //우측
+            p = new Pen(lineColor, 1);
+
+            //대각성 방향으로 이동하면서 십자가 모양의 선 그리기
+            for (int i= rectSize+ rectSize / 2;i< rectSize * edgeCount - rectSize / 2; i += rectSize)
+            {
+                gp.DrawLine(p, rectSize / 2, i, rectSize * edgeCount - rectSize / 2, i);
+                gp.DrawLine(p, i, rectSize / 2, i, rectSize * edgeCount - rectSize / 2);
+
+            }
+        }
+    }
+}
